ec3fb1e343f9aac4968fbdebe0c5a708dfd08fe1
Mohan <mohansisodiya50@gmail.com>
2020-07-20 21:11:10 +0530

    take 6

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
-import axios from 'axios';
 
 import App from './App.vue';
 import { routes } from './routes';
 import store from './store/store';
 
 Vue.use(VueRouter);
-
-const rootUrl = "https://vue-stock-trader-702d1.firebaseio.com/";
 
 Vue.filter('currency', value => {
 	return '$' + value.toLocaleString();
