dfb56914e89a6e37efe4749831c7e46bccb025b8
Mohan <mohansisodiya50@gmail.com>
2020-07-20 17:07:35 +0530

    take 3

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: process.env.NODE_ENV === 'production' ?
-    '/ca-tco/' : '/',
+    '/vue-stocks/' : '/',
     filename: 'build.js'
   },
   module: {
