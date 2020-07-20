dabf1e4d50864fdbc40a8bd9f2083d277b5958a0
Mohan <mohansisodiya50@gmail.com>
2020-07-20 16:57:37 +0530

    take 2

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: process.env.NODE_ENV === 'production' ?
+    '/ca-tco/' : '/',
     filename: 'build.js'
   },
   module: {
